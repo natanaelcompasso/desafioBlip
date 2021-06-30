d290c3389bb22b606ac1c8217adc3775b2d8956f
Natanael Garcia da Silva <natanael.silva@pampa.compasso>
2021-06-30 10:29:10 -0300

    Coloca o avatar no json

--- src/utils/repositorios.js
+++ src/utils/repositorios.js
@@ -6,6 +6,7 @@ module.exports = (json) =>{
         for (let i = 0; i < 5; i++) {
             repositorios.push({
                 "id": json[i].id,
+                "avatar_url": json[i].owner.avatar_url,
                 "nome": json[i].name,
                 "description": json[i].description
             })
